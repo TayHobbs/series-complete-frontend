50201bf36afd86f85bb57b5aabca5f0e96c518ca
TayHobbs <hobbstay@gmail.com>
2016-09-12 21:10:47 -0500

    Refactor: Clean up series card

--- js/series-card.js
+++ js/series-card.js
@@ -3,14 +3,10 @@ import React from 'react';
 export default React.createClass({
   render() {
     var render = [];
-    for (var i=0; i < this.props.data.installments.length; i++) {
-      let seriesId = `${this.props.data.title.replace(/ /, '-').toLowerCase()}-series-${i}`;
-      if (this.props.data.installments[i].checked) {
-        render.push(<div id={seriesId} key={this.props.data.installments[i].name}><input type="checkbox" checked /> {this.props.data.installments[i].name}</div>);
-      } else {
-        render.push(<div id={seriesId} key={this.props.data.installments[i].name}><input type="checkbox" /> {this.props.data.installments[i].name}</div>)
-      }
-    }
+    this.props.data.installments.forEach((installment) => {
+      let seriesId = `${this.props.data.title.replace(/ /, '-').toLowerCase()}-series-${installment.id}`;
+      render.push(<div id={seriesId} key={installment.name}><input type="checkbox" checked={installment.checked} /> {installment.name}</div>);
+    });
     return (
       <div>
         <div className='series-card'>
